8993eda071e1d3e9f2205954558de9f259fe054d
Archana8695 <sarchana348@gmail.com>
2022-04-04 23:05:40 +0530

    checkpoint 3 - Displaying the Upcoming and Released Movies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from "./common/header/Header";
+
+import Home from "./screens/home/Home";
 import "./index.css";
 
 ReactDOM.render(
     <React.StrictMode>
-    <Header />
+    
+    <Home />
   </React.StrictMode>,
   document.getElementById("root")
 );
